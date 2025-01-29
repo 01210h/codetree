e12b02b6614b7e231ee88c20bd28d1cd45ec76c4
이리안 <164438971+01210h@users.noreply.github.com>
2025-01-29 23:20:07 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250129/중앙값 구하기/find-the-median.cpp
+++ 250129/중앙값 구하기/find-the-median.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
     int a,b,c;
     cin>>a>>b>>c;
-    int mid=b;
     if(b<a){
         swap(a,b);//무조건 a<b
-        mid=b;
-    }else if(c<b&&a<c){
+    }
+    int mid=b;
+    if(c<b&&a<c){
         mid=c;
     }else if(c<b&&c<a){
         mid=a;
